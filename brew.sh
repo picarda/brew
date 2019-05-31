f9ec9bc0a1d3e51dff9f82f3392adde510e31ce8
Alain Picard <alain@alainpicard.ca>
2019-05-31 16:35:46 -0400

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install firefox
+brew cask install sublime-text
+brew cask install virtualbox
+brew cask install sourcetree
+brew cask install discord
+brew cask install skype
+brew cask install gimp
+brew cask install hyperdock
+brew cask install divvy
+brew cask install Asciiquarium
+brew cask install Neofetch
+brew cask install speedtest-cli
+brew cask install Ansiweather
+brew cask install Cmatrix
+brew cask install wpscan
+brew cask install sslyze
+brew cask install skipfish
+brew cask install nikto
+brew cask install nmap
+brew cask install sqlmap
+brew cask install flawfinder
+brew cask install afi-fuzz
+brew cask install bro
+brew cask install dcfldd
+brew cask install Lynis
+brew cask install mc
+brew cask install gisto
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew cask install font-source-code-pro
